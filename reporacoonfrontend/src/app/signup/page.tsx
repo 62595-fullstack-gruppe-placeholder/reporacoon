744d290525a1dbf0c074a8cb108ceea0a53a2e45
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-02-12 18:17:54 +0100

    fix: build time error in signup page

--- reporacoonfrontend/src/app/signup/page.tsx
+++ reporacoonfrontend/src/app/signup/page.tsx
@@ -2,12 +2,18 @@ import { getAllUsers } from "@/lib/repository/user/userRepository";
 import { SignupForm } from "./SignupForm";
 
 export default async function Signup() {
-  const users = await getAllUsers()
-  return (
-    <main>
-      <h1>This is a signup page</h1>
-      <p>current users: {JSON.stringify(users)}</p>
-      <SignupForm />
+  try {
+    const users = await getAllUsers();
+    return (
+      <main>
+        <h1>This is a signup page</h1>
+        <p>current users: {JSON.stringify(users)}</p>
+        <SignupForm />
+      </main>
+    );
+  } catch {
+    return <main>
+      <h1>An unknown error occured</h1>
     </main>
-  );
+  }
 }
