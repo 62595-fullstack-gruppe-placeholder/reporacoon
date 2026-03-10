23aec73c1a9d25e6acf788e66daa03f570c38709
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-03-10 18:54:27 +0100

    feat: meta title

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,11 +2,16 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { getUser } from "@/lib/auth/userFromToken";
 import Header from "./_components/Header";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+
+export const metadata: Metadata = {
+  title: "Repo Racoon",
+}
 
 export default async function RootLayout({
   children,
