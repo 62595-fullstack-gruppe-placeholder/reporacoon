1e0cf0532623cb1ac258e9e314ca3b7c67b3d141
Tobias Møller Larsen <Tobias-alleroed@hotmail.com>
2026-03-14 15:21:46 +0100

    fixed tests failing due to sonner using "matchMedia"

--- web/vitest.setup.ts
+++ web/vitest.setup.ts
@@ -1,2 +1,20 @@
 // vitest.setup.ts
 import '@testing-library/jest-dom';
+
+
+// Mock "matchMedia" because the sonner component requires it, and without the mock tests will fail.
+import { vi } from "vitest";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),           // deprecated
+    removeListener: vi.fn(),        // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
