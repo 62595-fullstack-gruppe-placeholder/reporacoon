ae81c5eff0a68682ea4b8325819dff33d18d23bf
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-03-15 11:48:16 +0100

    fix: build err

--- web/src/app/dashboard/account/page.tsx
+++ web/src/app/dashboard/account/page.tsx
@@ -1,8 +1,12 @@
 import { getUser } from "@/lib/auth/userFromToken";
-import { User } from "@/lib/repository/user/userSchemas";
 import { AccountDashboard } from "./PageContent";
 
 export default async function Page() {
-  const user = (await getUser()) as User;
+  const user = await getUser();
+
+  if (!user) {
+    return <div>You must be logged in to view this page.</div>
+  }
+
   return <AccountDashboard user={user} />;
 }
