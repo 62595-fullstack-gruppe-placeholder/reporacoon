bbd64e8f377aed49fe3e072928ec0f9ec1408621
Bønnelykke <93318173+August-Boennelykke@users.noreply.github.com>
2026-03-18 14:04:19 +0100

    now redirecting to dashboard

--- web/src/app/signup/page.tsx
+++ web/src/app/signup/page.tsx
@@ -1,5 +1,11 @@
+import { getUser } from "@/lib/auth/userFromToken";
 import { SignupForm } from "./SignupForm";
+import { redirect } from "next/navigation";
 
 export default async function Signup() {
+  let user = await getUser()
+    if (user) {
+      redirect("/dashboard")
+    }
   return <SignupForm />;
 }
